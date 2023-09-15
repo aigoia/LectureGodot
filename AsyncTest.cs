d92a6e38439a7de87c34a351d4f776acf953794a
aigoia <oidoicompany@gmail.com>
2023-09-15 11:22:09 +0900

    fix

--- AsyncTest.cs
+++ AsyncTest.cs
@@ -8,6 +8,7 @@ public partial class AsyncTest : Node2D
 	int DelayTime => 16;
 	float RotateAngle => 1f;
 	Node2D _icon;
+	bool _isRotate = true;
 	
 	public override void _Ready()
 	{
@@ -18,7 +19,7 @@ public partial class AsyncTest : Node2D
 
 	async void AsyncRoutine()
 	{
-		while (true)
+		while (_isRotate)
 		{
 			_icon.RotationDegrees += RotateAngle;
 			await Task.Delay(DelayTime);
