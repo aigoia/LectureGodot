59e8375ef59ae04c18acab4731eef66ae8a30e2e
aigoia <oidoicompany@gmail.com>
2023-09-18 09:59:32 +0900

    switch test

--- EgoScript/Map.cs
+++ EgoScript/Map.cs
@@ -4,7 +4,19 @@ namespace GodotTest.EgoScript;
 
 public partial class Map : Sprite2D
 {
-	public override void _Ready() => GD.Print("Hello World!");
+	public override void _Ready() => GD.Print(Say("Hello Godot!"));
+	
+	string Say(string say)
+	{
+		return (say.ToLower()) switch 
+		{
+			"hello world!" => "Nice to meet you!",
+			"hello world" => "Nice to meet you",
+			"hello godot!" => "Good to see you again!",
+			"hello godot" => "Good to see you again",
+			_ => say
+		};
+	}
 }
 
 
