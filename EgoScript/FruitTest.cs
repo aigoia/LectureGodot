b12a0aa3a05aef88d81796c0f72fa58bac092091
aigoia <oidoicompany@gmail.com>
2023-09-18 11:01:50 +0900

    cleaning

--- EgoScript/FruitTest.cs
+++ EgoScript/FruitTest.cs
@@ -25,7 +25,7 @@ public partial class FruitTest : Node2D
 		fruits.RemoveAll(i => i.FruitColor == Colors.Yellow);
 		var fruit = fruits.Find(i => i.FruitType == FruitType.Apple);
 		
-		var isReal = (fruit.FruitType) switch
+		var isReal = fruit.FruitType switch
 		{
 			FruitType.Apple => true,
 			FruitType.Banana => false,
